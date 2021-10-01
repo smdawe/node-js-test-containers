1104659243688b6c9ea56d517cbad70b73ae8bed
Simon Dawe <simon.dawe@oodlefinance.com>
2021-10-01 13:41:38 +0100

    aws

--- __tests__/aws/Aws.test.js
+++ __tests__/aws/Aws.test.js
@@ -2,54 +2,34 @@ import { GenericContainer, Wait } from "testcontainers";
 
 describe("Test Containers With S3", () => {
   // Declare the conatiner we want to use
-  const elasticsearchContainer = new GenericContainer("localstack/localstack");
+  const localstackContainer = new GenericContainer("localstack/localstack");
 
-  let runningElasticsearchContainer;
-  let elasticsearchClient;
+  let runningLocalstackContainer;
 
   beforeAll(async () => {
-    runningElasticsearchContainer = await elasticsearchContainer
-      .withExposedPorts(ELASTICSEARCH_PORT) // Expose port 9200 on the conatiner
-      .withEnv("discovery.type", "single-node") // Tell elasticsearch that there is only one node
+    runningLocalstackContainer = await localstackContainer
+      .withExposedPorts(
+        8080,
+        4567,
+        4568,
+        4569,
+        4570,
+        4571,
+        4572,
+        4573,
+        4574,
+        4575,
+        4576,
+        4577,
+        4578
+      )
       .withWaitStrategy(Wait.forLogMessage("Active license is now [BASIC]")) // Wait for this log message which tells us that the service is running
       .start();
 
-    return runningElasticsearchContainer;
+    return runningLocalstackContainer;
   }, 30000); // Allow container sometime to start
 
-  beforeEach(async () => {
-    const elasticsearchPort =
-      runningElasticsearchContainer.getMappedPort(ELASTICSEARCH_PORT); // Get the port that has been mapped to 9200
-    const elasticsearchAddress = runningElasticsearchContainer.getHost(); // Get the address of the container
-    const elasticsearchEndpoint = `http://${elasticsearchAddress}:${elasticsearchPort}`;
-
-    console.log(`Elasticsearch is available on ${elasticsearchEndpoint}`);
-
-    elasticsearchClient = new Client({ node: elasticsearchEndpoint });
-
-    const x = await elasticsearchClient.indices.create({
-      index: ELASTICSEARCH_INDEX,
-      body: index,
-    }); // Create test index
-
-    await elasticsearchClient.index({
-      index: ELASTICSEARCH_INDEX,
-      body: data,
-      refresh: true,
-    });
-  });
-
-  afterEach(async () => {
-    await elasticsearchClient.indices.delete({ index: ELASTICSEARCH_INDEX }); // Delete test index
-  });
-
   it("query the index", async () => {
-    const res = await elasticsearch(
-      elasticsearchClient,
-      ELASTICSEARCH_INDEX,
-      "Corey Taylor"
-    );
-
-    expect(res.band).toBe("Slipknot");
+    expect(true).toBe(true);
   });
 });
